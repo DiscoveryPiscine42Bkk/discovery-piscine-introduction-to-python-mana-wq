2ddf5692618202c8a278c4f503c2ab988d92937e
mana-wq <mana072549@gmail.com>
2025-06-18 05:35:17 +0000

    submit 19

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,5 @@
+# play_with_arrays.py
+original_array = [1, 3, 5, 7, 9]
+new_array = [x + 2 for x in original_array]
+print("original array:", original_array)
+print("new array(each elemant +2):", new_array)
