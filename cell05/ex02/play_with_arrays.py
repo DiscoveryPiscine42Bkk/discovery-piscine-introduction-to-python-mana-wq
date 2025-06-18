657eb85a5e301c730f75ec49fb95c8f5eb723b46
mana-wq <mana072549@gmail.com>
2025-06-18 06:17:17 +0000

    submit 19

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,5 @@
 # play_with_arrays.py
 original_array = [1, 5, 9, 7, 9, -11, 13, 5, 7,]
 new_array = [x + 2 for x in original_array if x + 2 > 5]
-print("original array:",original_array)
-print("new array (each element +2 and  >5)",new_array)
+print(original_array)
+print(new_array)
