3f19f193a3258d5a4738fad01e22c726d91729d3
mana-wq <mana072549@gmail.com>
2025-06-18 15:54:14 +0000

    submit

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,6 +1,9 @@
 num1 = int(input("Enter the first numberm:"))
 num2 = int(input("Enter the second number:"))
 print(f"{num1} * {num2} = {num1 * num2}")
-if num1 * num2 > 0: print("The result is positive.")
-elif num1 * num2 < 0: print("The result is negative.")
-else: print("The result is positive and negative")
+if num1 * num2 > 0: 
+    print("The result is positive.")
+elif num1 * num2 < 0: 
+    print("The result is negative.")
+else: 
+    print("The result is positive and negative")
