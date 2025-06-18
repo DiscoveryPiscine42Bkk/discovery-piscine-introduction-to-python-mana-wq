ef6abf65c5fefe14607e2dc7afc3feef65decf89
mana-wq <mana072549@gmail.com>
2025-06-18 15:53:18 +0000

    submit

--- cell02/ex00/is_zero.py
+++ cell02/ex00/is_zero.py
@@ -1,3 +1,5 @@
 value =int(input())
-if value == 0 : print("this number is equal to zero")
-else : print("tihs number is different from zero")
+if value == 0 :
+     print("this number is equal to zero")
+else : 
+    print("tihs number is different from zero")
